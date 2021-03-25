9df6542aeac7e5825061dbbb2dc6fcaa6667eaf2
Matthew R. MacLeod <mmacleod@ieee.org>
2021-03-25 13:33:48 -0400

    Add license, adjust line setting working directory.

--- code/main.R
+++ code/main.R
@@ -1,8 +1,35 @@
+# MIT License
+# 
+# Original work Copyright (c) 2020 Gianluca Rinaldi
+# Modified work Copyright (c) 2021 Her Majesty the Queen in Right of Canada, Department of National Defence, 2021.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#   
+#   The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+
 ##################################################
-## Set workind directory and load packages
+## Set working directory and load packages
 ##################################################
 # Set the working directory to the local folder to which the git was downloaded
-setwd("/Github/covid_IFR_Lombardy/")
+# MRM - set to current directory. This may be different depending on where the user
+# downloads it to.
+setwd(".")
 rm(list = ls())
 required_packages <- c("OECD", "rjags", "ggplot2", "R2OpenBUGS", "coda", "data.table", 
                        "MCMCvis", "viridis", "ggsci", "RColorBrewer", "stargazer", "countrycode", 
